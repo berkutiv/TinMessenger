9135cd60726113a6b5ee2058e475743356f29f46
berkutiv <berkutiv@MacBook-Pro-Ivan.local>
2017-09-29 17:40:51 +0300

    second commit

--- TinMessenger/ProfileViewController.swift
+++ TinMessenger/ProfileViewController.swift
@@ -8,57 +8,106 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
-    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var mainImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet var descriptionLabel: UIView!
-    @IBOutlet weak var changeButton: UIButton!
-    @IBOutlet weak var moreButton: UIButton!
-
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var cameraImage: UIImageView!
+    @IBOutlet weak var cameraBackView: UIView!
+    @IBOutlet weak var cameraButton: UIButton!
     
+    var photoPicker : UIImagePickerController?
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        avatarImage.layer.cornerRadius = avatarImage.frame.height/2
-        avatarImage.clipsToBounds = true
+        print("viewDidLoad frame \(editButton.frame)")
     }
     
     override func viewWillAppear(_ animated: Bool)
     {
+        //фрейм выводится одинаковый. По идее, в методе viewDidLoad, фрейм должен отличаться, так как вью еще не подгрузилась полностью и не выставила свои размеры. В данном случае, возможно это из-за стэк вью, либо есть еще вот случай 
+        
         super.viewWillAppear(true)
+        cameraImage.tintColor = .white
+        print("viewWillAppear frame \(editButton.frame)")
         print("viewWillAppear")
-    }
-    
-    override func viewDidAppear(_ animated: Bool)
-    {
-        super.viewDidAppear(true)
-        print("viewDidAppear")
-    }
-    
-    override func viewWillLayoutSubviews()
-    {
-        super.viewWillLayoutSubviews()
-        print("viewWillLayoutSubviews")
+        cameraBackView.layer.cornerRadius = cameraBackView.frame.size.width/2
+        cameraBackView.clipsToBounds = true
+        mainImage.layer.cornerRadius = cameraBackView.frame.height/2
+        mainImage.clipsToBounds = true
+        editButton.setTitle("Редактировать", for: .normal)
+        editButton.layer.cornerRadius = 10
+        editButton.layer.borderWidth = 1
+        editButton.backgroundColor = .white
+        nameLabel.text = "Иван Беркут"
+        descriptionLabel.text = "Другой текст, который поменьше"
     }
 
-    override func viewDidLayoutSubviews()
+    @IBAction func cameraButtonPressed(_ sender: Any)
     {
-        super.viewDidLayoutSubviews()
-        print("viewDidLayoutSubviews")
+        print("Выбери изображение профиля")
+        photoPicker = UIImagePickerController()
+        let alert:UIAlertController=UIAlertController(title: "Choose Image", message: nil, preferredStyle: UIAlertControllerStyle.actionSheet)
+        let cameraAction = UIAlertAction(title: "Камера", style: UIAlertActionStyle.default)
+        {
+            UIAlertAction in
+            self.openCamera(picker: self.photoPicker!)
+        }
+        let gallaryAction = UIAlertAction(title: "Галерея", style: UIAlertActionStyle.default)
+        {
+            UIAlertAction in
+            self.openGallery(picker: self.photoPicker!)
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: UIAlertActionStyle.cancel)
+        {
+            UIAlertAction in
+        }
+        
+        alert.addAction(cameraAction)
+        alert.addAction(gallaryAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
-    override func viewWillDisappear(_ animated: Bool)
+    @IBAction func editButtonPressed(_ sender: Any)
     {
-        super.viewWillDisappear(true)
-        print("viewWillDisappear")
+        
     }
-    
-    override func viewDidDisappear(_ animated: Bool)
-    {
-        super.viewDidDisappear(true)
-        print("viewDidDisappear")
-    }
+}
 
+
+//MARK:- работа с фото
+extension ProfileViewController
+{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
+        if picker == photoPicker
+        {
+            let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+            mainImage.contentMode = .scaleAspectFill
+            mainImage.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func openCamera(picker: UIImagePickerController)
+    {
+        picker.delegate = self
+        picker.sourceType = UIImagePickerControllerSourceType.camera
+        picker.allowsEditing = false
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func openGallery(picker: UIImagePickerController)
+    {
+        picker.delegate = self
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        picker.allowsEditing = false
+        self.present(picker, animated: true, completion: nil)
+    }
 }
 
